98ff6e66ef5e994dca6ee7f651142a66de087405
ausioz <oz.ausi@gmail.com>
2022-07-22 23:26:33 +0700

    remake game rule add com layout interaction

--- app/src/main/java/com/ausioz/binarch2gamesuit/MainActivity.kt
+++ app/src/main/java/com/ausioz/binarch2gamesuit/MainActivity.kt
@@ -15,6 +15,8 @@ class MainActivity : AppCompatActivity() {
 
     private val pilihanSuit = arrayOf("Batu", "Gunting", "Kertas")
     private var status: String? = null
+    private var pemain1: String? = null
+    private var pemain2: String? = null
     private var _binding: ActivityMainBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,17 +33,20 @@ class MainActivity : AppCompatActivity() {
         _binding?.playerBtnBatu?.setOnClickListener {
             refreshPick()
             activePickBatu()
-            gameFlow(pilihanSuit[0])
+            pemain1 = pilihanSuit[0]
+            doRule()
         }
         _binding?.playerBtnGunting?.setOnClickListener {
             refreshPick()
             activePickGunting()
-            gameFlow(pilihanSuit[1])
+            pemain1 = pilihanSuit[1]
+            doRule()
         }
         _binding?.playerBtnKertas?.setOnClickListener {
             refreshPick()
             activePickKertas()
-            gameFlow(pilihanSuit[2])
+            pemain1 = pilihanSuit[2]
+            doRule()
         }
         _binding?.btnLockPick?.setOnClickListener {
             playTerminal()
@@ -54,7 +59,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun rule(pemain1: String?, pemain2: String?) {
+    fun doRule() {
+        pemain2 = pilihanSuit.random()
         // 0: Batu      1: Gunting      2:Kertas
         if (
             pemain1 == pilihanSuit[0] && pemain2 == pilihanSuit[1] ||
@@ -71,24 +77,6 @@ class MainActivity : AppCompatActivity() {
         } else {
             status = "DRAW!"
         }
-    }
-
-    fun gameFlow(pemain1: String?){
-        val pemain2 = pilihanSuit.random()
-        rule(pemain1,pemain2)
-    }
-
-    fun playTerminal() {
-        val Count: CountDownTimer = object : CountDownTimer(4000, 1000) {
-            override fun onTick(millisUntilFinished: Long) {
-                _binding?.textTerminal?.text = (millisUntilFinished/1000).toString()
-                Log.d("Countdown", "playTerminal: ${(millisUntilFinished/1000)}")
-            }
-            override fun onFinish() {
-                _binding?.textTerminal?.text = status
-            }
-        }
-        Count.start()
     }
 
     fun activePickBatu() {
@@ -115,14 +103,27 @@ class MainActivity : AppCompatActivity() {
         _binding?.btnLockPick?.text = "Kuci Pilihan\nKertas"
     }
 
+    fun inactivePickCom(){
+        _binding?.comBtnBatu?.setColorFilter(Color.LTGRAY)
+        _binding?.comBtnGunting?.setColorFilter(Color.LTGRAY)
+        _binding?.comBtnKertas?.setColorFilter(Color.LTGRAY)
+    }
+
+
     fun refreshPick() {
         _binding?.btnLockPick?.isVisible = false
         _binding?.playerBtnBatu?.setBackgroundColor(Color.TRANSPARENT)
         _binding?.playerBtnBatu?.colorFilter = null
+        _binding?.comBtnBatu?.setBackgroundColor(Color.TRANSPARENT)
+        _binding?.comBtnBatu?.colorFilter = null
         _binding?.playerBtnGunting?.setBackgroundColor(Color.TRANSPARENT)
         _binding?.playerBtnGunting?.colorFilter = null
+        _binding?.comBtnGunting?.setBackgroundColor(Color.TRANSPARENT)
+        _binding?.comBtnGunting?.colorFilter = null
         _binding?.playerBtnKertas?.setBackgroundColor(Color.TRANSPARENT)
         _binding?.playerBtnKertas?.colorFilter = null
+        _binding?.comBtnKertas?.setBackgroundColor(Color.TRANSPARENT)
+        _binding?.comBtnKertas?.colorFilter = null
     }
 
     fun disablePick() {
@@ -136,5 +137,34 @@ class MainActivity : AppCompatActivity() {
         _binding?.playerBtnGunting?.isClickable = true
         _binding?.playerBtnKertas?.isClickable = true
     }
+    fun comPickReveal(){
+        // 0: Batu      1: Gunting      2:Kertas
+        if (pemain2 == pilihanSuit[0]){
+            _binding?.comBtnBatu?.setBackgroundColor(resources.getColor(R.color.active))
+            _binding?.comBtnBatu?.colorFilter = null
+        }
+        else if(pemain2 == pilihanSuit[1]){
+            _binding?.comBtnGunting?.setBackgroundColor(resources.getColor(R.color.active))
+            _binding?.comBtnGunting?.colorFilter = null
+        }
+        else{
+            _binding?.comBtnKertas?.setBackgroundColor(resources.getColor(R.color.active))
+            _binding?.comBtnKertas?.colorFilter = null
+        }
+    }
+    fun playTerminal() {
+        inactivePickCom()
+        val count: CountDownTimer = object : CountDownTimer(3000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                _binding?.textTerminal?.text = (millisUntilFinished/1000).toString()
+                Log.d("Countdown", "playTerminal: ${(millisUntilFinished/1000)}")
+            }
+            override fun onFinish() {
+                _binding?.textTerminal?.text = status
+                comPickReveal()
+            }
+        }
+        count.start()
 
+    }
 }
