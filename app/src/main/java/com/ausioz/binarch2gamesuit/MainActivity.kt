5f48851fcc03defaa31cb079989dfac0af3c1328
ausioz <oz.ausi@gmail.com>
2022-07-24 10:04:12 +0700

    create log debug and refresh visibility during countdown

--- app/src/main/java/com/ausioz/binarch2gamesuit/MainActivity.kt
+++ app/src/main/java/com/ausioz/binarch2gamesuit/MainActivity.kt
@@ -34,18 +34,21 @@ class MainActivity : AppCompatActivity() {
             refreshPick()
             activePickBatu()
             pemain1 = pilihanSuit[0]
+            Log.d("Pemain 1 Input", "$pemain1")
             doRule()
         }
         _binding?.playerBtnGunting?.setOnClickListener {
             refreshPick()
             activePickGunting()
             pemain1 = pilihanSuit[1]
+            Log.d("Pemain 1 Input", "$pemain1")
             doRule()
         }
         _binding?.playerBtnKertas?.setOnClickListener {
             refreshPick()
             activePickKertas()
             pemain1 = pilihanSuit[2]
+            Log.d("Pemain 1 Input", "$pemain1")
             doRule()
         }
         _binding?.btnLockPick?.setOnClickListener {
@@ -103,12 +106,11 @@ class MainActivity : AppCompatActivity() {
         _binding?.btnLockPick?.text = "Kuci Pilihan\nKertas"
     }
 
-    fun inactivePickCom(){
+    fun inactivePickCom() {
         _binding?.comBtnBatu?.setColorFilter(Color.LTGRAY)
         _binding?.comBtnGunting?.setColorFilter(Color.LTGRAY)
         _binding?.comBtnKertas?.setColorFilter(Color.LTGRAY)
     }
-
 
     fun refreshPick() {
         _binding?.btnLockPick?.isVisible = false
@@ -132,36 +134,44 @@ class MainActivity : AppCompatActivity() {
         _binding?.playerBtnKertas?.isClickable = false
         _binding?.btnLockPick?.isVisible = false
     }
+
     fun enablePick() {
         _binding?.playerBtnBatu?.isClickable = true
         _binding?.playerBtnGunting?.isClickable = true
         _binding?.playerBtnKertas?.isClickable = true
     }
-    fun comPickReveal(){
+
+    fun comPickReveal() {
         // 0: Batu      1: Gunting      2:Kertas
-        if (pemain2 == pilihanSuit[0]){
+        if (pemain2 == pilihanSuit[0]) {
             _binding?.comBtnBatu?.setBackgroundColor(resources.getColor(R.color.active))
             _binding?.comBtnBatu?.colorFilter = null
-        }
-        else if(pemain2 == pilihanSuit[1]){
+        } else if (pemain2 == pilihanSuit[1]) {
             _binding?.comBtnGunting?.setBackgroundColor(resources.getColor(R.color.active))
             _binding?.comBtnGunting?.colorFilter = null
-        }
-        else{
+        } else {
             _binding?.comBtnKertas?.setBackgroundColor(resources.getColor(R.color.active))
             _binding?.comBtnKertas?.colorFilter = null
         }
     }
+
     fun playTerminal() {
         inactivePickCom()
         val count: CountDownTimer = object : CountDownTimer(3000, 1000) {
             override fun onTick(millisUntilFinished: Long) {
-                _binding?.textTerminal?.text = (millisUntilFinished/1000).toString()
-                Log.d("Countdown", "playTerminal: ${(millisUntilFinished/1000)}")
+                _binding?.textTerminal?.text = ((millisUntilFinished / 1000) + 1).toString()
+                Log.d("Countdown", "playTerminal: ${((millisUntilFinished / 1000)) + 1}")
+                _binding?.btnRefresh?.isClickable = false
+                _binding?.btnRefresh?.isVisible = false
             }
+
             override fun onFinish() {
                 _binding?.textTerminal?.text = status
                 comPickReveal()
+                Log.d("Pemain COM Input", "$pemain2")
+                Log.d("Hasil pertandingan,", "$status")
+                _binding?.btnRefresh?.isClickable = true
+                _binding?.btnRefresh?.isVisible = true
             }
         }
         count.start()
